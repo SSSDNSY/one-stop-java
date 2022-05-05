a79adda7c2419ea37e397c7d428c1ab1081ab3cd
pengzh <736259020@qq.com>
2022-05-05 21:18:59 +0800

    add:netty bio

---
+++ src/main/java/net/netty/demo1/bio/BioClient.java
@@ -0,0 +1,26 @@
+package net.netty.demo1.bio;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+/**
+ * @author fun.pengzh
+ * @class net.netty.demo1.bio.BioClient
+ * @desc
+ * @since 2022-05-04
+ */
+public class BioClient {
+
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("127.0.0.1", 8991);
+            System.out.println("client 启动");
+            BioClientHandler bioClientHandler = new BioClientHandler(socket, Charset.forName("utf-8"));
+            bioClientHandler.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/main/java/net/netty/demo1/bio/BioClientHandler.java
+++ src/main/java/net/netty/demo1/bio/BioClientHandler.java
@@ -7,20 +7,21 @@ import java.util.Date;
 
 /**
  * @author fun.pengzh
- * @class net.netty.demo1.bio.BioServerHander
+ * @class net.netty.demo1.bio.BioClientHandler
  * @desc
- * @since 2022-05-03
+ * @since 2022-05-04
  */
-public class BioServerHandler extends ChannelAdapter {
+public class BioClientHandler extends ChannelAdapter {
 
-    public BioServerHandler(Socket socket, Charset charset) {
+
+    public BioClientHandler(Socket socket, Charset charset) {
         super(socket, charset);
     }
 
     @Override
     public void channelActive(ChannelHandler ctx) {
         System.out.println("链接报告LocalAddress:" + ctx.socket().getLocalAddress());
-        ctx.writeAndFlush("hi!  BioServer to msg for you \r\n");
+        ctx.writeAndFlush("hi! 我是bugstack虫洞栈 BioClient to msg for you \r\n");
     }
 
     @Override
@@ -28,5 +29,4 @@ public class BioServerHandler extends ChannelAdapter {
         System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + " 接收到消息：" + msg);
         ctx.writeAndFlush("hi 我已经收到你的消息Success！\r\n");
     }
-
 }
