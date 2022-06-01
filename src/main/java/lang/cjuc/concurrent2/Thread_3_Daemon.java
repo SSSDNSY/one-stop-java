f773a5d6d2ba538e7299b030d194dcb0c154e1bd
彭智慧 <pengzh666@163.com>
2022-06-01 14:01:38 +0800

    feat:new thread demo

---
+++ src/main/java/lang/cjuc/concurrent2/Thread_3_Daemon.java
@@ -0,0 +1,34 @@
+package lang.cjuc.concurrent2;
+
+import java.time.LocalTime;
+
+/**
+ * @author pengzh
+ * @desc
+ * @class ThreadDaemon
+ * @since 2022-06-01
+ */
+public class Thread_3_Daemon {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new TimerThread();
+        t.setDaemon(true);
+        t.start();
+        Thread.sleep(1100);
+    }
+
+}
+
+class TimerThread extends Thread {
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println(LocalTime.now());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+}
