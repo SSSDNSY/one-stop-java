761d650246d8f837d434d51f5731484f9ee0ca43
彭智慧 <sssdnsy@163.com>
2023-06-09 10:29:59 +0800

    feat: 双轴快速排序

--- data-algo/src/main/java/sort/SortUtils.java
+++ data-algo/src/main/java/sort/SortUtils.java
@@ -109,8 +109,8 @@ public class SortUtils {
      * @createDate: 2019/6/8 17:54
      */
     public static void shellSort(int[] arr) {
-        long t0 = System.currentTimeMillis();
-        int inc = arr.length / 2;
+        long t0  = System.currentTimeMillis();
+        int  inc = arr.length / 2;
         while (true) {
             inc /= 2;
             for (int i = 0; i < inc; i++) {
@@ -169,6 +169,160 @@ public class SortUtils {
 
     }
 
+    /**
+     * 双轴快速排序入口方法
+     *
+     * @param a
+     */
+    public static void sort(int[] a) {
+        sort(a, 0, a.length - 1, null, 0, 0);
+    }
+
+    /**
+     * 快速排序主方法:
+     * <p>
+     * a：要排序的整型数组。这是待排序数组的输入。
+     * <p>
+     * left：指定要排序的子数组的起始索引。
+     * <p>
+     * right：指定要排序的子数组的结束索引。
+     * <p>
+     * work：辅助数组，可用于在排序过程中进行临时存储或其他用途。可以为null，表示不使用辅助数组。
+     * <p>
+     * workBase：辅助数组中存储数据的起始索引。
+     * <p>
+     * workLen：辅助数组中存储数据的长度。
+     */
+    public static void sort(int[] a, int left, int right, int[] work, int workBase, int workLen) {
+        int length = right - left;
+
+        // 使用插入排序优化小规模数组
+        if (length < 27) {
+            for (int i = left + 1; i <= right; i++) {
+                for (int j = i; j > left && a[j] < a[j - 1]; j--) {
+                    swap(a, j, j - 1);
+                }
+            }
+            return;
+        }
+
+        int seventh = (length >> 3) + (length >> 6) + 1;
+
+        int e3 = (left + right) >>> 1; // 中间位置元素
+        int e2 = e3 - seventh;
+        int e1 = e2 - seventh;
+        int e4 = e3 + seventh;
+        int e5 = e4 + seventh;
+
+        // 进行五点取样并排序
+        if (a[e2] < a[e1]) {
+            swap(a, e2, e1);
+        }
+        if (a[e3] < a[e2]) {
+            swap(a, e3, e2);
+            if (a[e2] < a[e1]) {
+                swap(a, e2, e1);
+            }
+        }
+        if (a[e4] < a[e3]) {
+            swap(a, e4, e3);
+            if (a[e3] < a[e2]) {
+                swap(a, e3, e2);
+                if (a[e2] < a[e1]) {
+                    swap(a, e2, e1);
+                }
+            }
+        }
+        if (a[e5] < a[e4]) {
+            swap(a, e5, e4);
+            if (a[e4] < a[e3]) {
+                swap(a, e4, e3);
+                if (a[e3] < a[e2]) {
+                    swap(a, e3, e2);
+                    if (a[e2] < a[e1]) {
+                        swap(a, e2, e1);
+                    }
+                }
+            }
+        }
+
+        int pivot1 = a[e2];
+        int pivot2 = a[e4];
+
+        // 进行划分
+        swap(a, e2, left);
+        swap(a, e4, right);
+
+        int less  = left + 1;
+        int great = right - 1;
+
+        for (int k = less; k <= great; k++) {
+            if (a[k] < pivot1) {
+                swap(a, k, less++);
+            } else if (a[k] > pivot2) {
+                while (k < great && a[great] > pivot2) {
+                    great--;
+                }
+                swap(a, k, great--);
+                if (a[k] < pivot1) {
+                    swap(a, k, less++);
+                }
+            }
+        }
+
+        int dist = less - left;
+        if (dist < 3) {
+            swap(a, left, less - 1);
+        } else {
+            swap(a, left, less - 1);
+            swap(a, left + 1, less);
+        }
+        dist = right - great;
+        if (dist < 3) {
+            swap(a, right, great + 1);
+            swap(a, right - 1, great);
+        } else {
+            swap(a, right, great + 1);
+            swap(a, right - 1, great);
+            swap(a, right - 2, great - 1);
+        }
+
+        sort(a, left, less - 2, work, workBase, workLen);
+        sort(a, great + 2, right, work, workBase, workLen);
+
+        // 对等于主元的元素进行插入排序
+        if (less < e1 && e5 < great) {
+            while (a[less] == pivot1) {
+                less++;
+            }
+            while (a[great] == pivot2) {
+                great--;
+            }
+
+            outer:
+            for (int k = less; k <= great; k++) {
+                if (a[k] == pivot1) {
+                    swap(a, k, less++);
+                } else if (a[k] == pivot2) {
+                    while (a[great] == pivot2) {
+                        if (great-- == k) {
+                            break outer;
+                        }
+                    }
+                    if (a[great] == pivot1) {
+                        swap(a, k, less++);
+                    } else {
+                        swap(a, k, great--);
+                    }
+                }
+            }
+        }
+
+        // 对等于主元的元素进行排序
+        sort(a, less, great, work, workBase, workLen);
+    }
+
+
     public static void showQuickSort(int size) {
         stepList = new ArrayList<int[]>();
         quickSort(generateArray(size), 0, size - 1);
@@ -183,11 +337,11 @@ public class SortUtils {
         long t0 = System.currentTimeMillis();
         if (first < last) {
             int middle = (first + last) / 2;
-            //左半部分排好序
+            // 左半部分排好序
             mergeSort(arr, first, middle, temp);
-            //右半部分排好序
+            // 右半部分排好序
             mergeSort(arr, middle + 1, last, temp);
-            //合并左右部分
+            // 合并左右部分
             mergeArray(arr, first, middle, last, temp);
             showGraph(arr);
         }
@@ -226,7 +380,7 @@ public class SortUtils {
      */
     public static void radixSort(int[] arr) {
         long t0 = System.currentTimeMillis();
-        //最浪费空间的一种方法
+        // 最浪费空间的一种方法
         int[] maxArr = new int[0x0000ffff];
         for (int i = 0; i < arr.length; i++) {
             maxArr[arr[i]] = arr[i];
@@ -244,8 +398,8 @@ public class SortUtils {
      * @createDate: 2019/6/6 15:35
      */
     public static void MinHeapSort(int a[], int n) {
-        long t0 = System.currentTimeMillis();
-        int temp = 0;
+        long t0   = System.currentTimeMillis();
+        int  temp = 0;
         MakeMinHeap(a, n);
 
         for (int i = n - 1; i > 0; i--) {
@@ -259,7 +413,7 @@ public class SortUtils {
         showArray(a);
     }
 
-    //构建最小堆
+    // 构建最小堆
     public static void MakeMinHeap(int a[], int n) {
         for (int i = (n - 1) / 2; i >= 0; i--) {
             MinHeapFixdown(a, i, n);
@@ -272,12 +426,12 @@ public class SortUtils {
      * @createDate: 2019/6/8 21:19
      */
     public static void MinHeapFixdown(int a[], int i, int n) {
-        //子节点
-        int j = 2 * i + 1;
+        // 子节点
+        int j    = 2 * i + 1;
         int temp = 0;
 
         while (j < n) {
-            //在左右子节点中寻找最小的
+            // 在左右子节点中寻找最小的
             if (j + 1 < n && a[j + 1] < a[j]) {
                 j++;
             }
@@ -285,7 +439,7 @@ public class SortUtils {
                 break;
             }
 
-            //较大节点下移
+            // 较大节点下移
             temp = a[i];
             a[i] = a[j];
             a[j] = temp;
@@ -296,7 +450,7 @@ public class SortUtils {
     }
 
     /**
-     * @description:
+     * @description: 交换数组中两个元素的位置
      * @author: pengzh
      * @createDate: 2019/6/6 10:45
      */
@@ -328,7 +482,7 @@ public class SortUtils {
         if (size < 1) {
             return null;
         }
-        int max = -1, min = -1;
+        int   max = -1, min = -1;
         int[] arr = new int[size];
         for (int i = 0; i < size; i++) {
             arr[i] = new Random().nextInt(size);
@@ -349,7 +503,7 @@ public class SortUtils {
     }
 
     public static int[] genrateIncArray(int size, int step) {
-        long t0 = System.currentTimeMillis();
+        long  t0  = System.currentTimeMillis();
         int[] arr = new int[size];
         for (int i = 0; i < size; i++) {
             arr[i] += i * step;
