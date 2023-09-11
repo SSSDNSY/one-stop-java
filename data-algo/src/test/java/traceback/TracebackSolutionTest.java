2537ac2992348a40b43062e2649bd236f78eb646
彭智慧 <736259020@qq.com>
2023-09-11 19:11:37 +0800

    feat: algo Traceback 回溯2

--- data-algo/src/test/java/traceback/TracebackSolutionTest.java
+++ data-algo/src/test/java/traceback/TracebackSolutionTest.java
@@ -25,5 +25,14 @@ public class TracebackSolutionTest {
         List<List<Integer>> combine = solution.combine(4, 2);
         assert  combine.size()==6;
     }
+    @Test
+    public void TestCombineSum(){
+        //示例: 输入: n = 4, k = 2 输出: [ [2,4], [3,4], [2,3], [1,2], [1,3], [1,4],]
+        List<List<Integer>> combine = solution.combineSum(9, 3);
+        System.out.println(combine);
+        assert  combine.size()==3;
+    }
+
+
 
 }
