b91add2f3560bcd7e64fc219769f5ca22d94b40e
彭智慧 <736259020@qq.com>
2023-09-19 21:07:08 +0800

    feat: algo greedy 贪心算法

--- data-algo/src/main/java/greedy/Solution.java
+++ data-algo/src/main/java/greedy/Solution.java
@@ -6,6 +6,13 @@ import java.util.Arrays;
  * @Desc 贪心
  * @Author pengzh
  * @Since 2023-09-18
+ *
+ * 不好意思，贪心没套路，就刷题而言，如果感觉好像局部最优可以推出全局最优，然后想不到反例，那就试一试贪心吧！
+ *
+ * 而严格的数据证明一般有如下两种：
+ *
+ * 数学归纳法
+ * 反证法
  */
 public class Solution {
 
@@ -40,6 +47,46 @@ public class Solution {
             }
         }
         return count;
+    }
+
+    /**
+     * 摆动序列
+     */
+    public int wiggleMaxLength(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        // 当前差值
+        int curDiff = 0;
+        // 上一个差值
+        int preDiff = 0;
+        // 出现次数
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            curDiff = nums[i] - nums[i - 1];
+            // 波动判定
+            if ((curDiff > 0 && preDiff <= 0) || (curDiff < 0 && preDiff >= 0)) {
+                count++;
+                preDiff = curDiff;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * 最大子序列和
+     */
+    public int maxSubseqSum(int[] nums) {
+        int sum  = Integer.MIN_VALUE;
+        int temp = 0;
+        for (int i = 0; i < nums.length; i++) {
+            temp += nums[i];
+            sum = Math.max(temp, sum);
+            if (temp <= 0) {
+                temp = 0;
+            }
+        }
+        return sum;
     }
 
 }
--- data-algo/src/test/java/greedy/GreedySolutionTest.java
+++ data-algo/src/test/java/greedy/GreedySolutionTest.java
@@ -13,17 +13,30 @@ public class GreedySolutionTest {
     private Solution solution;
 
     @Before
-    public void before(){
+    public void before() {
         solution = new Solution();
     }
 
     @Test
     public void testChildrenAndCookie() {
-        int num = solution.cookieAndChildren2(new int[]{1,2}, new int[]{1,2,3});
+        int num = solution.cookieAndChildren2(new int[]{1, 2}, new int[]{1, 2, 3});
         System.out.println(num);
         assert num == 2;
     }
 
+    @Test
+    public void testWiggleMaxLength() {
+        int count = solution.wiggleMaxLength(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
+        System.out.println(count);
+        assert count == 2;
+    }
+
+    @Test
+    public void testMaxSubsetSum() {
+        int maxSum = solution.maxSubseqSum(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
+        System.out.println(maxSum);
+        assert maxSum == 6;
+    }
 
 
 }
