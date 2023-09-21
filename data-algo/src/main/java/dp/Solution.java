b6c553f69882a138bf9c940098f7be3c81036a6d
彭智慧 <736259020@qq.com>
2023-09-21 10:57:17 +0800

    feat: algo dp 动态规划

--- data-algo/src/main/java/dp/Solution.java
+++ data-algo/src/main/java/dp/Solution.java
@@ -7,6 +7,78 @@ package dp;
  */
 public class Solution {
 
+
+    /**
+     * 斐波那契数列
+     */
+    public int feb1(int n) {
+        if (n < 2) {
+            return n;
+        }
+        return feb1(n - 1) + feb1(n - 2);
+    }
+
+    public int feb2(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int a = 0, b = 1, c = 0;
+        for (int i = 1; i < n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+
+    public int feb3(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int[] nums = new int[n + 1];
+        nums[0] = 0;
+        nums[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            nums[i] = nums[i - 1] + nums[i - 2];
+        }
+        return nums[n];
+    }
+
+    /**
+     * 爬楼梯
+     */
+    public int claimStairs(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+    public int claimStairs2(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int[] dp = new int[3];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            int sum =dp[1]+dp[2];
+            dp[1]=dp[2];
+            dp[2]=sum;
+        }
+        return dp[n];
+    }
+
+
+
+    /**
+     * 打家劫舍
+     */
     public int rob1(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
         if (nums.length == 1) return nums[0];
---
+++ data-algo/src/test/java/dp/DynamicProgrammingSolutionTest.java
@@ -0,0 +1,32 @@
+package dp;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @Desc 动态规划单元测试
+ * @Author pengzh
+ * @Since 2023-09-21
+ */
+public class DynamicProgrammingSolutionTest {
+
+    private Solution solution;
+
+    @Before
+    public void before() {
+        solution = new Solution();
+    }
+
+    @Test
+    public void testFeb() {
+        // 0 1 1 2 3 5 8 13 21 34 55
+        int i1 = solution.feb1(3);
+        int i2 = solution.feb2(8);
+        int i3 = solution.feb3(10);
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+        assert i1 == 2 && i2 == 21 && i3 == 55;
+    }
+
+}
