08e6176703b91a38e0f809f77f751f4c7ea4b543
彭智慧 <736259020@qq.com>
2023-11-23 18:39:59 +0800

    feat: algo dp rob2

--- data-algo/src/main/java/dp/Solution.java
+++ data-algo/src/main/java/dp/Solution.java
@@ -484,4 +484,24 @@ public class Solution {
     }
 
 
+    /**
+     * 打家劫舍II
+     */
+    public int rob2(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        return Math.max(rob2(nums, 0, nums.length - 2), rob2(nums, 1, nums.length - 1));
+    }
+
+    public int rob2(int[] nums, int start, int end) {
+        int[] dp = new int[nums.length];
+        dp[start] = nums[start];
+        dp[start+1] = Math.max(dp[start], nums[start+1]);
+        for (int i = start+2; i < end; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[end];
+    }
+
+
 }
