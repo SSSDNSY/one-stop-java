173a499f17f395bf1308b4c5d7c4db3d82083b89
彭智慧 <736259020@qq.com>
2024-04-17 11:33:09 +0800

    feat: swing FontPrinter

---
+++ sundries/src/main/java/FontPrinter.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+
+/**
+ * @Desc
+ * @Author pengzh
+ * {@link }
+ * @Since 2024-04-17
+ */
+public class FontPrinter {
+
+    public static void main(String[] args) {
+        GraphicsEnvironment e = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        String[] fontNames = e.getAvailableFontFamilyNames();
+        for(String name:fontNames){
+            System.out.println(name);
+        }
+    }
+
+}
