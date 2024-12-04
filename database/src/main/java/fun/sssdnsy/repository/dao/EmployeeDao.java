3a69d6e9df92aa054fbddd0a89af7ff69aa23d09
彭智慧 <736259020@qq.com>
2024-12-04 15:07:53 +0800

    refact: remove duplicate declare

--- database/src/main/java/fun/sssdnsy/repository/dao/EmployeeDao.java
+++ database/src/main/java/fun/sssdnsy/repository/dao/EmployeeDao.java
@@ -13,13 +13,5 @@ import java.util.Map;
  */
 public interface EmployeeDao extends MyBatisDao<String, EmployeeCO> {
 
-    /**
-     * 查询实体对象
-     *
-     * @param map 通用查询对象
-     * @return 分页结果
-     */
-    List<EmployeeCO> query(Map<String, Object> map);
-
 
 }
