bf1cb6448740fb8185c61951daea479485f605de
彭智慧 <736259020@qq.com>
2024-12-11 10:53:00 +0800

    feat: database , QueryFilter optimization

--- database/src/main/java/fun/sssdnsy/common/query/FieldRelation.java
+++ database/src/main/java/fun/sssdnsy/common/query/FieldRelation.java
@@ -2,11 +2,6 @@ package fun.sssdnsy.common.query;
 
 /**
  * 查询条件之间的组合关系
- *
- * @author heyifan
- * @company 广州宏天软件股份有限公司
- * @email heyf@jee-soft.cn
- * @date 2018年4月4日
  */
 public enum FieldRelation {
 
